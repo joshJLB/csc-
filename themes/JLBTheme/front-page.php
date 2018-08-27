8fbbcc35ff27aa2f84fe23ebb86e2213d6147ebe
unknown <josh@jlbworks.com>
2018-08-27 14:42:53 -0500

    Initial setup.

---
+++ themes/JLBTheme/front-page.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  Template Name: Home Page
+  front-page.php
+*/
+get_header(); ?>
+
+<main id="home">
+
+  <?php get_template_part('components/home-page/hero'); ?>
+  <!-- Or, nah -->
+  <section class="hero">
+
+  </section>
+
+  <!-- Continue Sections -->
+  <h1>Hello World</h1>
+</main>
+
+<?php get_footer();

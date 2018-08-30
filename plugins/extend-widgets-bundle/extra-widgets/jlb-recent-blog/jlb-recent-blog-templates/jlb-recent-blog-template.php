6791f65623e5a09679494bcae1982f988c59ec07
unknown <josh@jlbworks.com>
2018-08-30 14:10:57 -0500

    Added recent blog widget and mobilized

---
+++ plugins/extend-widgets-bundle/extra-widgets/jlb-recent-blog/jlb-recent-blog-templates/jlb-recent-blog-template.php
@@ -0,0 +1,28 @@
+<?php
+$image = wp_get_attachment_url($instance['recent_blog_image']);
+$title = $instance['recent_blog_title'];
+$link = $instance['recent_blog_link_title'];
+?>
+
+<div class="recent-blog-widget-container">
+    <div class="six-container">
+      <div class="blog-section-circle" style="background-image:url('http://localhost/wp-content/uploads/2018/08/YELLOW-CIRCLE-FOR-BLOG.png')">
+        <h1><?=$title; ?></h1>
+      </div>
+      <div class="blog-section blog-section-image" style="background-image:url(<?=$image; ?>)"></div>
+      <div class="blog-section blog-section-content">
+        <?php
+	        $args = array( 'numberposts' => '1' );
+            $recentPosts = wp_get_recent_posts( $args );
+            $mostRecent = $recentPosts[0];
+        ?>
+        <div class="blog-section-content-inner">
+          <h1><?=$mostRecent['post_title']?></h1>
+          <div class="blog-section-body">
+            <p><?=$mostRecent['post_content']?></p>
+          </div>
+          <a href="<?=get_permalink($mostRecent['ID']); ?>"><?=$link?> >></a>
+        </div>
+      </div>
+    </div>
+  </div>
